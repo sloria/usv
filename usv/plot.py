c5340057af142cb378d6f237a8f8df18e42ed42c
Steven Loria <sloria1@gmail.com>
2012-10-09 15:24:54 -0500

    Fix errors in Plotter

--- usv/plot.py
+++ usv/plot.py
@@ -5,28 +5,37 @@ from glob import glob
 import pylab as pl
 import numpy as np
 
+def all_plots():
+    plots = {}
+    plots['full'] = Plotter('bigarena/full')
+    plots['first15'] = Plotter('bigarena/first15')
+    plots['first5'] = Plotter('bigarena/first5')
+    return plots
+
 class Plotter(object):
     def __init__(self, folder):
         n_fm_list = []
         n_hfm_list = []
         n_sv_list = []
+        
         p_fm_list = []
         p_hfm_list = []
         p_sv_list = []
-        datafiles = glob(os.path.join(folder, 'ppd*.[tT][aA][bB]'))
+        datafiles = glob(os.path.join(folder, '*.[tT][aA][bB]'))
         datafiles.sort()
         for datafile in datafiles:
             with open(datafile, 'r') as data:
                 lines = data.readlines()
                 counts = lines[len(lines) - 2]
                 freqs = lines[len(lines) - 1]
-                tokens = counts.split('\t')
-                n_fm_list.append(float(tokens[2]))
-                n_hfm_list.append(float(tokens[3]))
-                n_sv_list.append(float(tokens[4]))
-                p_fm_list.append(float(tokens[2]))
-                p_hfm_list.append(float(tokens[3]))
-                p_sv_list.append(float(tokens[4]))
+                count_tokens = counts.split('\t')
+                freq_tokens = freqs.split('\t')
+                n_fm_list.append(float(count_tokens[2]))
+                n_hfm_list.append(float(count_tokens[3]))
+                n_sv_list.append(float(count_tokens[4]))
+                p_fm_list.append(float(freq_tokens[2]))
+                p_hfm_list.append(float(freq_tokens[3]))
+                p_sv_list.append(float(freq_tokens[4]))
         self.ppds = [0, 3, 7, 10, 14, 17, 21, 24, 28]
         self.n_fm = np.array(n_fm_list)
         self.n_hfm = np.array(n_hfm_list)
@@ -36,21 +45,22 @@ class Plotter(object):
         self.p_sv = np.array(p_sv_list)
         
     def plot_counts(self):
+        figure()
         bar_width = 0.65
         x_pos = np.arange(len(self.ppds))
-        p1 = pl.bar(x_pos, self.n_fm, width, color='r', label='n FM')
-        p2 = pl.bar(x_pos, self.n_hfm, width, color='g', label='n HFM', bottom=self.n_fm)
-        p3 = pl.bar(x_pos, self.n_sv, width, color='b', label='n SV', bottom=(self.n_fm + self.n_hfm))
+        p1 = pl.bar(x_pos, self.n_fm, bar_width, color='r', label='n FM')
+        p2 = pl.bar(x_pos, self.n_hfm, bar_width, color='g', label='n HFM', bottom=self.n_fm)
+        p3 = pl.bar(x_pos, self.n_sv, bar_width, color='b', label='n SV', bottom=(self.n_fm + self.n_hfm))
         pl.title('Counts of each call type over time (PPD)')
         pl.ylabel('Count')
         pl.xlabel('PPD')
-        pl.xticks(x_pos+width/2., ppds)
+        pl.xticks(x_pos + bar_width/2., self.ppds)
         pl.legend()
         print "Opening bar plot..."
-        pl.show()
+        pl.ion()
 
     def plot_freqs(self):
-        pl.figure()
+        figure()
         pl.title('Call type frequencies over time (PPD)')
         pl.xlabel('PPD')
         pl.ylabel('p')
@@ -60,4 +70,4 @@ class Plotter(object):
         pl.grid(True)
         pl.legend()
         print "Opening frequency plot..."
-        pl.show()
+        pl.ion()
